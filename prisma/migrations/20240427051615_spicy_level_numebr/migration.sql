8df074ea07a3bd9c018887fecdf364960a9d448b
Erick <esantostaype@gmail.com>
2024-05-02 13:33:24 -0500

    postgres

--- prisma/migrations/20240427051615_spicy_level_numebr/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `orderproducts` ADD COLUMN `spicyLevel` INTEGER NULL;
-
--- AlterTable
-ALTER TABLE `product` ADD COLUMN `spicyLevelNumber` INTEGER NULL;
--- prisma/migrations/20240427061109_spicy_level_number/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `spicyLevel` on the `orderproducts` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `orderproducts` DROP COLUMN `spicyLevel`,
-    ADD COLUMN `spicyLevelNumber` INTEGER NULL;
