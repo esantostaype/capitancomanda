442ccb2fdba356aefa29cc45a7805c3c7bf8e222
Erick <esantostaype@gmail.com>
2024-09-19 15:27:29 -0500

    print3

--- src/components/ui/TextFieldWithRef.tsx
+++
@@ -1,8 +0,0 @@
-import React, { forwardRef, Ref } from 'react'
-import { TextField as OriginalTextField, TextFieldProps } from '@/components'
-
-export const TextFieldWithRef = forwardRef<HTMLInputElement, TextFieldProps>(
-  (props, ref) => <OriginalTextField innerRef={ref} {...props} />
-)
-
-TextFieldWithRef.displayName = 'TextFieldWithRef'
